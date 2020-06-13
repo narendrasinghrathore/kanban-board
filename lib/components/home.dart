b848dff8babb15b53e2642e7e3dc590aa67e2b35
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-06-13 22:19:13 +0530

    Row - Container - Text Creating a block

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
@@ -13,8 +14,81 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+        primary: true,
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Stack(
+            children: <Widget>[
+              SvgPicture.asset('assets/images/notes.svg'),
+              ListView(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Center(
+                          child: Text(
+                            'Some text.',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        width: 100.0,
+                        height: 100.0,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadiusDirectional.circular(4),
+                          color: Colors.white12,
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            'Some text.',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        width: 100.0,
+                        height: 100.0,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadiusDirectional.circular(4),
+                          color: Colors.white12,
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            'Some text.',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        width: 100.0,
+                        height: 100.0,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadiusDirectional.circular(4),
+                          color: Colors.white12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
