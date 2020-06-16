630eb1de7d9d7c50bc281895606596c172280173
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-06-16 21:29:34 +0530

    common settings for color and fonts.

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -8,6 +8,13 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
   final String title;
+  final borderColors = Color.fromRGBO(65, 180, 158, 0.5);
+  static final fontSize = 16.0;
+  double _volume = 0.0;
+  final textStyle = TextStyle(
+    color: Colors.white,
+    fontSize: fontSize,
+  );
 }
 
 class _HomeState extends State<Home> {
@@ -30,27 +37,71 @@ class _HomeState extends State<Home> {
           margin: EdgeInsets.all(10),
           child: Stack(
             children: <Widget>[
-              // SvgPicture.asset('assets/images/notes.svg'),
+              SvgPicture.asset('assets/images/notes.svg'),
               ListView(
                 children: [
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.volume_up),
+                            tooltip: 'Increase volume by 10',
+                            onPressed: () {
+                              setState(() {
+                                widget._volume += 10;
+                              });
+                            },
+                          ),
+                          Text('Volume : $widget._volume',
+                              style: widget.textStyle)
+                        ],
+                      ),
                       Container(
                         child: Center(
-                          child: Text(
-                            'Add New Note',
-                            style: TextStyle(
-                              color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Add New Note',
+                              style: widget.textStyle,
                             ),
                           ),
                         ),
-                        width: 200.0,
-                        height: 300.0,
+                        width: 100.0,
+                        height: 100.0,
                         margin: EdgeInsets.all(10),
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadiusDirectional.circular(4),
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            style: BorderStyle.solid,
+                            width: 2,
+                            color: widget.borderColors,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Setting',
+                              style: widget.textStyle,
+                            ),
+                          ),
+                        ),
+                        width: 100.0,
+                        height: 100.0,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            style: BorderStyle.solid,
+                            width: 2,
+                            color: widget.borderColors,
+                          ),
                         ),
                       ),
                     ],
